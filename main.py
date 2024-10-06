3b503dacf9d533ef2806e5586b50fdd95882cc9c
abdelilahElgharbaoui <a.elgharbaoui1@gmail.com>
2024-10-06 11:07:54 +0100

    data ingestion notebook added

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
+from textSummarizer.pipline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from textSummarizer.logging import logger
 
-logger.info("welcome to our custom logging")
+STAGE_NAME = "Data Ingestion stage"
+try:
+    logger.info(f"{STAGE_NAME} started.")
+    pipeline = DataIngestionTrainingPipeline()
+    pipeline.main()
+    logger.info(f"{STAGE_NAME} completed successfully.")
+except Exception as e:
+    logger.exception(e)
+    raise e
---
+++ src/textSummarizer/config/configuration.py
@@ -0,0 +1,34 @@
+from textSummarizer.constants import *
+from textSummarizer.utils.common import read_yaml, create_directories
+from textSummarizer.entity import (DataIngestionConfig,)
+
+from textSummarizer.logging import logger
+class ConfigurationManager:
+    def  __init__(
+        self,
+        config_filepath = CONFIG_FILE_PATH,
+        params_filepath = PARAMS_FILE_PATH):
+        
+        try:
+            
+            self.config = read_yaml(Path("config/config.yaml"))
+        except FileNotFoundError as e:
+            logger.error(f"Configuration file not found: {config_filepath}")
+            raise e
+        self.params = read_yaml(params_filepath)
+        
+        create_directories([self.config.artifacts_root])
+        
+    def get_data_ingestion_config(self) -> DataIngestionConfig:
+        config = self.config.data_ingestion
+        
+        create_directories([config.root_dir])
+        data_ingestion_config = DataIngestionConfig(
+            root_dir=config.root_dir,
+            source_url=config.source_url,
+            local_data_file=config.local_data_file,
+            unzip_dir=config.unzip_dir,
+        )
+        return data_ingestion_config
+    
+    
